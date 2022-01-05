ae960f7043cc2d7e69d9e2d513a017cf50e270bf
wcartaya <williams.cartaya@mercadolibre.com>
2022-01-05 15:57:02 -0300

    section2 complete

--- app/app.go
+++ app/app.go
@@ -12,8 +12,10 @@ func Start() {
 	//router := http.NewServeMux()
 	router := mux.NewRouter()
 	//routers
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	//starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
